8166ee8fe543fdae8e908a921dfde7f51377939c
Dongato15 <114643470+Dongato15@users.noreply.github.com>
2022-09-29 11:53:54 -0500

    Update Dongatobot.js

--- Dongatobot.js
+++ Dongatobot.js
@@ -1,26 +1,95 @@
-const { WAConnection } = require('@adiwajshing/baileys');
-const fs = require('fs');
+const {
+	default: makeWASocket,
+	useSingleFileAuthState,
+	DisconnectReason,
+	getContentType
+} = require('@adiwajshing/baileys')
+const fs = require('fs')
+const P = require('pino')
+const qrcode = require('qrcode-terminal')
+const util = require('util')
 
-async function iniciar () { 
-        const client = new WAConnection()
+const { state, saveState } = useSingleFileAuthState('./session.json')
 
-        client.logger.level = 'warn'
+const prefix = '.'
+const staff = ['5219984907794']
 
-        client.on('qr', () => {
-        })
+const connectToWA = () => {
+	const conn = makeWASocket({
+		logger: P({ level: 'silent' }),
+		printQRInTerminal: true,
+		auth: state,
+	})
+	
+	conn.ev.on('connection.update', (update) => {
+		const { connection, lastDisconnect } = update
+		if (connection === 'close') {
+			if (lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+				connectToWA()
+			}
+		} else if (connection === 'open') {
+			console.log('Bot conectado')
+		}
+	})
+	
+	conn.ev.on('creds.update', saveState)
+	
+	conn.ev.on('messages.upsert', async(mek) => {
+		try {
+			mek = mek.messages[0]
+			if (!mek.message) return
+			
+			mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+			if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+			const type = getContentType(mek.message)
+			const content = JSON.stringify(mek.message)
+			const from = mek.key.remoteJid
+			
+			const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+			const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
+			
+			const isCmd = body.startsWith(prefix)
+			const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+			
+			const args = body.trim().split(/ +/).slice(1)
+			const q = args.join(' ')
+			const isGroup = from.endsWith('@g.us')
+			const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
+			const senderNumber = sender.split('@')[0]
+			const botNumber = conn.user.id.split(':')[0]
+			const pushname = mek.pushName || 'Sin Nombre'
+			
+			const isMe = botNumber.includes(senderNumber)
+			const isStaff = staff.includes(senderNumber) || isMe
+			
+			const reply = (teks) => {
+				conn.sendMessage(from, { text: teks }, { quoted: mek })
+			}
+			
+			switch (command) {
 
-        fs.existsSync('./Samu330.json') && client.loadAuthInfo('./Samu330.json')
+case 'hola':
+reply('que hay xD')
+break
 
-        client.on('connecting', () => {
-        console.log('Conectando')
-        })
+				default:
+					
+					if (isStaff && body.startsWith('>')) {
+						try {
+							await reply(util.format(await eval(`(async () => {${body.slice(1)}})()`)))
+						} catch(e) {
+							await reply(util.format(e))
+						}
+					}
+					
+			}
+			
+		} catch (e) {
+			const isError = String(e)
+			
+			console.log(isError)
+		}
+	})
+}
 
-        client.on('open', () => {
-        console.log('Conectado exitosamente :D')
-        })
-        await client.connect({timeoutMs: 30*1000})
-        fs.writeFileSync('./Samu330.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
-        }
-
-iniciar ()
-.catch (err => console.log("unexpected error: " + err))
+connectToWA()
